9712436c9d2b9451dcf1d5645bbf2b64d816d8db
Carlos Serôdio <carlosserodio86@hotmail.com>
2022-08-11 09:03:32 +0100

    footer changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,6 +33,7 @@ import UserEditScreen from './screens/UserEditScreen';
 import SellerRoute from './components/SellerRoute';
 import SellerScreen from './screens/SellerScreen';
 import SearchBox from './components/SearchBox';
+import Footer from './components/Footer';
 import SearchScreen from './screens/SearchScreen';
 import MapScreen from './screens/MapScreen';
 import AboutScreen from './screens/AboutScreen';
@@ -354,154 +355,8 @@ function App() {
 
             {userInfo && !userInfo.isAdmin && <ChatBox userInfo={userInfo} />}
             <div class="text-center"><i class="fas fa-ellipsis-h"></i></div>
-            
 
-<footer className='bg-dark'>
-  <div className="container">
-    <div className="row text-center mb-3">
-
-
-
-     <div className="col-md-4 mb-3">
-
-        <ul className="nav flex-column">
-          <li class="nav-item">
-            <span class="footer-title">Product</span>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Product 1</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Product 2</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Plans & Prices</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Frequently asked questions</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="col-md-4 mb-3">
-        <ul class="nav flex-column">
-          <li className="nav-item">
-            <span class="footer-title">Company</span>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About us</a>
-          </li>
-          <li className="nav-item">
-            <a class="nav-link" href="#">Job postings</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">News and articles</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="col-md-4 mb-3">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <span className="footer-title">Contact & Support</span>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="https://wa.me/00351967389659" target="_blank"><i className="fas fa-phone"></i>+351 967 389 659</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="https://wa.me/00351967389659"><i className="fas fa-comments"></i>Live chat</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#"><i className="fas fa-envelope"></i>Contact us</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#"><i className="fas fa-star"></i>Give feedback</a>
-          </li>
-          
-        </ul>
-      </div> 
-    </div>
-
-    {/* <div className="text-center"><i className="fas fa-ellipsis-h"></i></div> */}
-
-    <div className="row text-center mb-3">
-    <div class="col-md-12 box">
-        <ul class="list-inline social-buttons">
-        {/* <li class="list-inline-item">
-            <a href="https://www.instagram.com/">
-            <i class="fab fa-instagram"></i>
-          </a>
-          </li>          
-          <li class="list-inline-item">
-            <a href="https://www.facebook.com/">
-            <i class="fab fa-facebook"></i>
-          </a>
-          </li>  */}
-          <li class="list-inline-item">
-            <a href="https://twitter.com/serodio_carlos" target="_blank" >
-            <i class="fab fa-twitter"></i>
-          </a>
-          </li>
-          <li class="list-inline-item">
-            <a href="https://github.com/csalpha" target="_blank" >
-            <i class="fab fa-github"></i>
-          </a>
-          </li>
-          <li class="list-inline-item">
-            <a href="linkedin.com/in/carlos-serôdio-0b3a05230" target="_blank" >
-            <i class="fab fa-linkedin-in"></i>
-          </a>
-          </li>
-          {/* <li class="list-inline-item">
-            <a href="https://www.google.pt/search?ved=1t:65428&_ga=2.15921462.1215513500.1660145777-1981032716.1655712712&q=Carlos+Ser%C3%B4dio&ludocid=7134482983659233008&lsig=AB86z5WqXbAvS8l2oDK9PIKWnEhd#trex=m_t:lcl_akp,rc_ludocids:7134482983659233008,rc_q:Carlos%2520Ser%25C3%25B4dio,ru_gwp:0%252C7,ru_q:Carlos%2520Ser%25C3%25B4dio,trex_id:OdFIlc">
-            <i class="fab fa-google"></i>
-          </a>
-          </li> */}
-          <li class="list-inline-item">
-            <a href="https://www.youtube.com/channel/UCJtSl1227biHNWp2fnSBQdQ" target="_blank" >
-            <i class="fab fa-youtube"></i>
-          </a>
-          </li>
-          <li class="list-inline-item">
-            <a href="https://wa.me/00351967389659" target="_blank" >
-            <i class="fab fa-whatsapp"></i>
-          </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    
-    
-
-    
-    
-    <div className="row text-center">
-
-      <div class="col-md-12 box">
-        <ul className=" list-inline quick-links">
-          <li className="list-inline-item">
-            <a className="footer-link" href="#">Conditions of Use</a>
-          </li>
-          <li className="list-inline-item">
-            <a className="footer-link" href="#">Privacy Notice</a>
-          </li>
-          <li className="list-inline-item">
-            <a className="footer-link" href="#">Interest-Based Ads</a>
-          </li>
-          <li className="list-inline-item">
-          <span class="copyright quick-links"> &copy; {new Date().getFullYear()} Copyright:{' Carlos Serôdio'}
-        </span>
-        </li>
-        </ul>
-
-      </div>
-      
-    </div>
-  </div>
-</footer>
-
-
+            <Footer />
     </div>
 
     </BrowserRouter>
