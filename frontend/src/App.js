ff18ee612d77daaefacc05fe471ccfef92ab9cc9
Carlos Serôdio <carlosserodio86@hotmail.com>
2022-09-13 10:46:53 +0100

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -95,29 +95,6 @@ function App() {
     fetchCategories();
   }, [ctxDispatch]);
 
-  // let mybutton;
-
-  // window.onscroll = function () {
-  //   mybutton = document.getElementById('btn-back-to-top');
-  //   scrollFunction(mybutton);
-  // };
-
-  // function scrollFunction(mybutton) {
-  //   if (
-  //     document.body.scrollTop > 20 ||
-  //     document.documentElement.scrollTop > 20
-  //   ) {
-  //     mybutton.style.display = 'block';
-  //   } else {
-  //     mybutton.style.display = 'none';
-  //   }
-  // }
-
-  // function backToTop() {
-  //   document.body.scrollTop = 0;
-  //   document.documentElement.scrollTop = 0;
-  // }
-
   return (
     <BrowserRouter>
       <div
@@ -402,14 +379,6 @@ function App() {
 
             <Route path="/" element={<HomeScreen />}></Route>
           </Routes>
-
-          {/* <Button
-            onClick={backToTop}
-            id="btn-back-to-top"
-            className="back-to-top"
-          >
-            <MDBIcon fas icon="arrow-up" />
-          </Button> */}
         </main>
 
         {userInfo && !userInfo.isAdmin && <ChatBox userInfo={userInfo} />}
